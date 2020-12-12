1f241b8b41c25242de07ce22b375c4ee0b690c46
Oscar Eg Gensmann <oscar@gensmann.dk>
2020-12-12 19:36:40 +0100

    wip

--- start.sh
+++ start.sh
@@ -5,12 +5,6 @@ set -eu
 # ensure that data directory is owned by 'cloudron' user
 chown -R cloudron:cloudron /app/data
 
-if [[ ! -f /app/data/directus/.env ]]; then
-  echo "copy .env"  
-  cp /app/data/directus/example.env  /app/data/directus/.env
-  echo "Done."
-fi
-
 echo "==> Starting"
 exec /usr/local/bin/gosu cloudron:cloudron npm start
 
