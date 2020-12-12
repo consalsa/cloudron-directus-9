95ac4ef2e28a62f6cd1d43651a707319c7c403df
Oscar Eg Gensmann <oscar@gensmann.dk>
2020-12-12 02:35:02 +0100

    wip

--- start.sh
+++ start.sh
@@ -5,15 +5,9 @@ set -eu
 # ensure that data directory is owned by 'cloudron' user
 chown -R cloudron:cloudron /app/data
 
-if [[ -f /app/data/setenv.sh ]]; then
-  echo "Set environment Variables"
-  cp /app/data/setenv.sh /app/code
-  chmod +x /app/code/setenv.sh
-  echo "Done."
-fi
-
 if [[ ! -f /app/data/.bootstrapped ]]; then
   echo "Run bootstrap"
+  exec /app/data/setenv.sh
   exec npm run bootstrap
   touch /app/data/.bootstrapped
   echo "Done."
